1c67a8d8a3f0cea065fc188fae349425c5b04df1
KarinaBabak <karisha.babak@gmail.com>
2017-10-19 01:14:30 +0300

    create solution

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWorld;
+
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Please input your minds: ");
+                    inputWorld = Console.ReadLine();
+                    if(String.IsNullOrWhiteSpace(inputWorld))
+                    {
+
+                    }
+                    Console.WriteLine(inputWorld[0]);
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Please type something. Do not ignore!");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("ArgumentException");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
