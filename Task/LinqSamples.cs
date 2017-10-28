57ca915d8b469d517644e423f9d6bbba53c9f51a
KarinaBabak <karisha.babak@gmail.com>
2017-10-28 18:56:47 +0300

    done

--- Task/LinqSamples.cs
+++ Task/LinqSamples.cs
@@ -13,53 +13,54 @@ using System.Windows.Forms;
 using System.Xml.Linq;
 using SampleSupport;
 using Task.Data;
+using System.Text.RegularExpressions;
 
 // Version Mad01
 
 namespace SampleQueries
 {
-	[Title("LINQ Module")]
-	[Prefix("Linq")]
-	public class LinqSamples : SampleHarness
-	{
+    [Title("LINQ Module")]
+    [Prefix("Linq")]
+    public class LinqSamples : SampleHarness
+    {
 
-		private DataSource dataSource = new DataSource();
+        private DataSource dataSource = new DataSource();
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 1")]
-		[Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
-		public void Linq_1()
-		{
-			int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        [Category("Restriction Operators")]
+        [Title("Where - Task 1")]
+        [Description("This sample uses the where clause to find all elements of an array with a value less than 5.")]
+        public void Linq_1()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-			var lowNums =
-				from num in numbers
-				where num < 5
-				select num;
+            var lowNums =
+                from num in numbers
+                where num < 5
+                select num;
 
-			Console.WriteLine("Numbers < 5:");
-			foreach (var x in lowNums)
-			{
-				Console.WriteLine(x);
-			}
-		}
+            Console.WriteLine("Numbers < 5:");
+            foreach (var x in lowNums)
+            {
+                Console.WriteLine(x);
+            }
+        }
 
-		[Category("Restriction Operators")]
-		[Title("Where - Task 2")]
-		[Description("This sample return return all presented in market products")]
+        [Category("Restriction Operators")]
+        [Title("Where - Task 2")]
+        [Description("This sample return return all presented in market products")]
 
-		public void Linq_2()
-		{
-			var products =
-				from p in dataSource.Products
-				where p.UnitsInStock > 0
-				select p;
+        public void Linq_2()
+        {
+            var products =
+                from p in dataSource.Products
+                where p.UnitsInStock > 0
+                select p;
 
-			foreach (var p in products)
-			{
-				ObjectDumper.Write(p);
-			}
-		}
+            foreach (var p in products)
+            {
+                ObjectDumper.Write(p);
+            }
+        }
 
         [Category("Restriction Operators")]
         [Title("Where - Task 4")]
@@ -98,15 +99,15 @@ namespace SampleQueries
                     }
                 );
 
-            foreach (var p in result)
+            foreach (var c in result)
             {
-                ObjectDumper.Write(p.Customer);
+                ObjectDumper.Write(c.Customer);
 
-                foreach (var s in p.Suppliers)
+                foreach (var s in c.Suppliers)
                 {
                     ObjectDumper.Write(s.SupplierName);
                 }
-                
+
             }
         }
 
@@ -116,9 +117,183 @@ namespace SampleQueries
 
         public void Linq2_WithOutGroupJoin()
         {
-            
-
+            foreach (var customer in dataSource.Customers)
+            {
+                ObjectDumper.Write(customer.CompanyName);
+                ObjectDumper.Write(
+                    dataSource.Suppliers
+                        .Where(s => s.Country == customer.Country && s.City == customer.City)
+                        .Select(s => s.SupplierName)
+                );
+            }
         }
 
+        [Category("Restriction Operators")]
+        [Title("Where - Task 7")]
+        [Description("This sample return clients who has order with price more than X")]
+
+        public void Linq3()
+        {
+            decimal maxPrice = 9000;
+            ObjectDumper.Write(dataSource.Customers
+                .Where(c => c.Orders.Any(o => o.Total > maxPrice))
+                .Select(c => c.CompanyName));
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 8")]
+        [Description("This sample return clients with date of their first ordering")]
+
+        public void Linq4()
+        {
+            ObjectDumper.Write(
+                dataSource.Customers
+                    .Select(c => new {
+                        customer = c.CompanyName,
+                        firstOrder = c.Orders.Any() ? c.Orders.Min(o => o.OrderDate) : (DateTime?)null
+                    })
+            );
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 9")]
+        [Description("This sample return clients with date of their first ordering")]
+
+        public void Linq5()
+        {
+            ObjectDumper.Write(
+                dataSource.Customers
+                    .Select(c => new {
+                        customerName = c.CompanyName,
+                        totalSumOfOrders = c.Orders.Any() ? c.Orders.Sum(o => o.Total) : 0,
+                        firstOrder = c.Orders.Any() ? c.Orders.Min(o => o.OrderDate) : (DateTime?)null
+                    })
+                    .OrderBy(o => o.firstOrder.HasValue ? o.firstOrder.Value.Year : 0)
+                    .ThenBy(o => o.firstOrder.HasValue ? o.firstOrder.Value.Month : 0)
+                    .ThenByDescending(o => o.totalSumOfOrders)
+                    .ThenBy(o => o.customerName)
+            );
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 10")]
+        [Description("This sample return clients without region, phone extension and with non diginal postal code")]
+
+        public void Linq6()
+        {
+            var degitalRegex = "^[0-9]+$";
+            var customers = dataSource.Customers.Where(
+                c => (!String.IsNullOrEmpty(c.PostalCode) && !Regex.IsMatch(c.PostalCode, degitalRegex))
+                || String.IsNullOrWhiteSpace(c.Region)
+                || c.Phone[0] != '(');
+            foreach (var customer in customers)
+            {
+                ObjectDumper.Write($"{customer.CompanyName} {customer.PostalCode} {customer.Region} {customer.Phone}");
+            }
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 11")]
+        [Description("This sample return groupped products")]
+
+        public void Linq7()
+        {
+            var result = dataSource.Products
+                .GroupBy(p => p.Category)
+                .Select(category =>
+                    {
+                        var grouppedByCount =
+                            category.GroupBy(c => c.UnitsInStock).OrderBy(u => u.Count());
+                        var grouppedByPrice = grouppedByCount.Select(i => i.OrderBy(p => p.UnitPrice));
+                        return new
+                        {
+                            category = category.Key,
+                            grouppedProducts = grouppedByPrice.ToList()
+                        };
+                    }
+                );
+
+            foreach (var item in result)
+            {
+                ObjectDumper.Write(item.category.ToUpper());
+                foreach (var product in item.grouppedProducts)
+                {
+                    ObjectDumper.Write(product);
+                }
+            }
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 12")]
+        [Description("This sample return groupped products by price")]
+
+        public void Linq8()
+        {
+            var result = dataSource.Products
+                .GroupBy(p =>
+                {
+                    if (p.UnitPrice < 10)
+                    {
+                        return "cheap";
+                    }
+                    else if (p.UnitPrice < 20)
+                    {
+                        return "not expensive";
+                    }
+
+                    return "expensive";
+                });
+
+            foreach (var groups in result)
+            {
+                ObjectDumper.Write(groups.Key.ToUpper());
+                foreach (var product in groups)
+                {
+                    ObjectDumper.Write(product);
+                }
+            }
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 13")]
+        [Description("This sample return cite revenue and sell intensity")]
+
+        public void Linq9()
+        {
+            var result = dataSource.Customers
+                .GroupBy(c => c.City)
+                .Select(group => new
+                {
+                    city = group.Key,
+                    sum = group.Where(c => c.Orders.Any()).Average(c => c.Orders.Sum(o => o.Total)),
+                    intensity = group.Sum(i => i.Orders.Count()) / group.Count()
+                });
+            ObjectDumper.Write(result);
+        }
+
+        [Category("Restriction Operators")]
+        [Title("Where - Task 14")]
+        [Description("This sample return statistic")]
+
+        public void Linq10()
+        {
+            var orders = dataSource.Customers.SelectMany(c => c.Orders);
+
+            foreach (var item in orders.GroupBy(o => o.OrderDate.Month))
+            {
+                ObjectDumper.Write(item);
+            }
+
+            foreach (var item in orders.GroupBy(o => o.OrderDate.Year))
+            {
+                ObjectDumper.Write(item);
+            }
+
+            foreach (var item in orders.GroupBy(o => new { o.OrderDate.Year, o.OrderDate.Month }))
+            {
+                ObjectDumper.Write(item);
+            }
+        }
     }
+
 }
